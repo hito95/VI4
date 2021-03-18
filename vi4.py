9ec411df1f12a3abefa1d64ce9879f799bd23ef9
Gael MATHIEU <gael.mathieu@hotmail.fr>
2021-03-18 15:25:00 +0100

    Ajout label titre

--- vi4.py
+++ vi4.py
@@ -7,7 +7,11 @@ ticker_id = "^FCHI"
 now = dt.datetime.now().strftime("20%y-%m-%d")
 ohlc = functions.get_data(ticker=ticker_id, interval="1d", start_date="2020-03-18", end_date=now)
 
-price = np.flip(ohlc["Close"])
+price_close = np.flip(ohlc["Close"])
 date = np.flip(ohlc["Date"])
 
-plt.plot(date, price)
+plt.xlabel("Dates")
+plt.ylabel("Close price of CAC 40")
+plt.title("Evolution of CAC40 close price")
+plt.plot(date, price_close)
+plt.show()
