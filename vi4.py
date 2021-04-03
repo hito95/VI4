50abde7072cbdfbdfffeeca6efbbaa4128e6c5b7
Gael MATHIEU <gael.mathieu@hotmail.fr>
2021-04-03 05:32:20 +0200

    Implémentation de la courbe efficient (rendement en fonction du risque) Problème lors de l'optimisation non linéaire de la volatilité du portefeuille sous contrainte la moyenne du portefeuille = une certaine valeur et la somme des poids = 1

--- vi4.py
+++ vi4.py
@@ -2,16 +2,102 @@ import functions
 import datetime as dt
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import minimize
 
-ticker_id = "^FCHI"
+ticker_id = "GLE.PA"
 now = dt.datetime.now().strftime("20%y-%m-%d")
 ohlc = functions.get_data(ticker=ticker_id, interval="1d", start_date="2020-03-18", end_date=now)
 
-price_close = np.flip(ohlc["Close"])
-date = np.flip(ohlc["Date"])
+price_close = list(np.flip(ohlc["Close"]))
+date = list(np.flip(ohlc["Date"]))
 
 plt.xlabel("Dates")
 plt.ylabel("Close price of CAC 40")
 plt.title("Evolution of CAC40 close price")
 plt.plot(date, price_close)
 plt.show()
+
+
+""" ------------------------------------------------- TEST ------------------------------------------------- """
+
+liste_ticker = ["VIV.PA","EN.PA","SAN.PA","ENGI.PA","AI.PA","ORA.PA","BN.PA","WLN.PA","BNP.PA","SGO.PA","MC.PA","LR.PA","ACA.PA","RI.PA","SW.PA","SU.PA","OR.PA","CA.PA","ML.PA","GLE.PA","AC.PA","FP.PA","KER.PA","CAP.PA","VIE.PA","DG.PA","HO.PA","AIR.PA","ATO.PA"]
+
+date = []
+price_close = []
+n = len(liste_ticker)
+
+
+for i in range(n):
+    now = dt.datetime.now().strftime("20%y-%m-%d")
+    ohlc = functions.get_data(ticker=liste_ticker[i], interval="1d", start_date="2016-03-18", end_date=now)
+    price_close.append(list(np.flip(ohlc["Close"])))
+
+date = list(np.flip(ohlc["Date"]))
+
+
+plt.figure("Figure 1")
+plt.xlabel("Dates")
+plt.ylabel("Close price ")
+plt.title("Evolution of close price")
+for i in range(n):
+    plt.plot(date, price_close[i])
+plt.show()
+
+yields = []
+
+for i in range(n):
+    tempo = np.zeros(len(price_close[i]))
+    tempo = list(tempo)
+    for j in range(1, len(price_close[i])):
+        tempo[j] = (price_close[i][j] - price_close[i][j - 1]) / price_close[i][j - 1]
+    yields.append(tempo)
+
+
+weight = []
+
+for i in range(n):
+    weight.append(1/n)
+
+def mean_portfolio(yields, weight):
+    n = len(yields)
+    
+    mean = []
+    for i in range(n):
+        mean.append(np.mean(yields[i]))
+    
+    return np.mean(mean)
+    
+    
+
+def var_portfolio(yields, weight):
+    n = len(yields)
+    
+    var = []
+    
+    for i in range(n):
+        var.append(np.var(yields[i]))
+        
+    covar = np.cov(yields)
+    
+    sum_ = 0
+    for i in range(n):
+        sum_2 = 0
+        for j in range(n):
+            sum_2 += weight[i]*weight[j]*covar[i,j]
+        sum_ += var[i]*weight[i]**2 + 2*sum_2
+    
+    return sum_
+        
+
+var_port = var_portfolio(yields, weight)
+
+def contraint1(weight):
+    return sum(weight) - 1
+
+def contraint2(yields, weight):
+    return mean_portfolio(yields, weight) - 1
+
+cons = [{'type':'eq', 'fun': contraint1},
+        {'type':'eq', 'fun': contraint2}]
+
+markowitz_limit = minimize(fun = var_portfolio, x0 = yields[0], args = weight, contraints=cons )
